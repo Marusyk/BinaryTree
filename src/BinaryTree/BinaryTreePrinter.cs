a90630347746a3da437dea366ede77f3a2a4d692
Mathias Rasmussen <mathiasvr@gmail.com>
2018-10-11 19:00:34 +0200

    Add BinaryTreePrinter

---
+++ src/BinaryTree/BinaryTreePrinter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public class BinaryTreePrinter<T> where T : IComparable<T>
+    {
+        const int maxValueLength = 5;
+        public void PrettyPrint(BinaryTreeNode<T> root)
+        {
+            PrettyPrintRec(root, 0);
+        }
+
+        private void PrettyPrintRec(BinaryTreeNode<T> node, int spaces)
+        {
+            if (node != null)
+            {
+                PrettyPrintRec(node.Left, spaces + 1);
+                Console.WriteLine(new String(' ', spaces * maxValueLength) + node.Value);
+                PrettyPrintRec(node.Right, spaces + 1);
+            }
+        }
+    }
+}
