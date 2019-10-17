e9e53bf37258a8b51047a461acd41cf020452310
Divakar Kumar <Divakar.Kumar@Lennoxintl.com>
2019-10-17 13:18:03 +0530

    abstract

--- src/BinaryTree/BinaryTree.cs
+++ src/BinaryTree/BinaryTree.cs
@@ -7,7 +7,7 @@ namespace BinaryTree
     public class BinaryTree<T> : ICollection<T> where T : IComparable<T>
     {
         private ITraversalStrategy<T> _traversalStrategy;
-        public BinaryTreeNode<T> _head;
+        private BinaryTreeNode<T> _head;
 
         public BinaryTree(ITraversalStrategy<T> traversalStrategy)
         {
