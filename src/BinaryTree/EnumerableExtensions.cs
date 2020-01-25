787f5bc7c22dccbe53f7398de75e395b829d2577
Divakar <divakar.divakar.k@gmail.com>
2020-01-26 04:16:36 +0530

    Print function for binary Tree traversal (#23)

--- src/BinaryTree/EnumerableExtensions.cs
+++ src/BinaryTree/EnumerableExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace BinaryTree
@@ -7,10 +7,100 @@ namespace BinaryTree
     {
         public static void PrintToConsole<T>(this IEnumerable<T> source)
         {
-            foreach (var item in source)
+            if (source is BinaryTree<int>)
             {
-                Console.Write($"{item} ");
+                BinaryTree<int> binaryTree = (BinaryTree<int>)source;
+                binaryTree.Print();
             }
+            else
+            {
+                foreach (var item in source)
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+
+            
         }
+
+        public static void Print(this BinaryTree<int> root, string textFormat = "0", int spacing = 1, int topMargin = 2, int leftMargin = 2)
+        {
+            if (root == null) return;
+            int rootTop = Console.CursorTop + topMargin;
+            var last = new List<NodeInfo>();
+            var next = root._head;
+            for (int level = 0; next != null; level++)
+            {
+                var item = new NodeInfo { Node = next, Text = next.Value.ToString(textFormat) };
+                if (level < last.Count)
+                {
+                    item.StartPos = last[level].EndPos + spacing;
+                    last[level] = item;
+                }
+                else
+                {
+                    item.StartPos = leftMargin;
+                    last.Add(item);
+                }
+                if (level > 0)
+                {
+                    item.Parent = last[level - 1];
+                    if (next == item.Parent.Node.Left)
+                    {
+                        item.Parent.Left = item;
+                        item.EndPos = Math.Max(item.EndPos, item.Parent.StartPos - 1);
+                    }
+                    else
+                    {
+                        item.Parent.Right = item;
+                        item.StartPos = Math.Max(item.StartPos, item.Parent.EndPos + 1);
+                    }
+                }
+                next = next.Left ?? next.Right;
+                for (; next == null; item = item.Parent)
+                {
+                    int top = rootTop + 2 * level;
+                    Print(item.Text, top, item.StartPos);
+                    if (item.Left != null)
+                    {
+                        Print("/", top + 1, item.Left.EndPos);                      
+                    }
+                    if (item.Right != null)
+                    {                        
+                        Print("\\", top + 1, item.Right.StartPos - 1);
+                    }
+                    if (--level < 0) break;
+                    if (item == item.Parent.Left)
+                    {
+                        item.Parent.StartPos = item.EndPos + 1;
+                        next = item.Parent.Node.Right;
+                    }
+                    else
+                    {
+                        if (item.Parent.Left == null)
+                            item.Parent.EndPos = item.StartPos - 1;
+                        else
+                            item.Parent.StartPos += (item.StartPos - 1 - item.Parent.EndPos) / 2;
+                    }
+                }
+            }
+            Console.SetCursorPosition(0, rootTop + 2 * last.Count - 1);
+        }
+
+        private static void Print(string s, int top, int left, int right = -1)
+        {
+            Console.SetCursorPosition(left, top);
+            if (right < 0) right = left + s.Length;
+            while (Console.CursorLeft < right) Console.Write(s);
+        }
+    }
+    class NodeInfo
+    {
+        public BinaryTreeNode<int> Node;
+        public string Text;
+        public int StartPos;
+        public int Size { get { return Text.Length; } }
+        public int EndPos { get { return StartPos + Size; } set { StartPos = value - Size; } }
+        public NodeInfo Parent, Left, Right;
     }
 }
