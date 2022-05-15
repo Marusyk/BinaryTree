cb7029f5cf459f97d36b90a3613a15356b6fa558
Roman Marusyk <Marusyk@users.noreply.github.com>
2022-05-16 00:37:43 +0300

    Update BinaryTree.cs

--- src/BinaryTree/BinaryTree.cs
+++ src/BinaryTree/BinaryTree.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace BinaryTree
-{
+namespace BinaryTree {
     public class BinaryTree<T> : ICollection<T> where T : IComparable<T>
     {
         private ITraversalStrategy<T> _traversalStrategy;
